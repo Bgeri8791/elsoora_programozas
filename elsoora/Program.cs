4d656ae907ba9509588febf8c982023e9963779f
Bgeri8791 <buzasi.geri@gmail.com>
2023-01-17 19:01:28 +0100

    feladat02

--- elsoora/Program.cs
+++ elsoora/Program.cs
@@ -34,6 +34,7 @@ namespace elsoora
                 van = true;
             }
             */
+            /*
             int i = 0;
             int idx = 0;
             bool van = false;
@@ -52,7 +53,35 @@ namespace elsoora
                 van = true;
                 Console.WriteLine("Van ilyen elem a tömbben, és indexe "+idx);
                 Console.ReadLine();
+            }*/
+            /*
+            //Megszámlálás tétel:
+            int db = 0;
+            int[] tomb = new int[] { 23, 43, 31, 3, 41 };
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i]%2 == 0)
+                {
+                    db++;
+                }
+                
             }
+            Console.WriteLine("Ennyi darab megfelelő elem van: " + db);
+            */
+            /*
+            int db = 0;
+            string[] tomb = new string[] { "alma","zöldalma","körte","sárgaalma"};
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i].Contains("alma"))
+                {
+                    db++;
+                }
+
+            }
+            Console.WriteLine("Hány 'alma' szó szerepel a tömbben: " + db);
+            Console.ReadLine();
+            */
         }
     }
 }
