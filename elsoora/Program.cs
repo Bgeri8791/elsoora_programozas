e385d4ca60141f5f77fd4fdd374bfff033137106
Bgeri8791 <buzasi.geri@gmail.com>
2023-01-17 18:50:13 +0100

    Add project files.

---
+++ elsoora/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elsoora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Összegzés tétel:
+            int osszeg = 0;
+            int[] tomb = new int[] { 1, 6, 32, 65, 10 };
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                osszeg = osszeg + tomb[i];
+            }
+            Console.WriteLine("A tömb összege: " + osszeg);
+            Console.ReadLine();
+            */
+            /*//Eldöntés tétele:
+            int i = 0;
+            bool van = false;
+            int[] tomb = new int[] { 23,43,-2,3,2 };
+            while (i <= tomb.Length && !(tomb[i] > 0)) 
+            {
+                i++;
+            }
+            if (i<= tomb.Length)
+            {
+                van = true;
+            }
+            */
+            //
+        }
+    }
+}
